0307b2dfdde4250814adacf5c2706ca5f388dff2
杨惠超 <fb208@outlook.com>
2021-12-16 15:33:07 +0800

    增加修改class文件的方法

--- src/main/java/com/fb208/jcode/ByteCode.java
+++ src/main/java/com/fb208/jcode/ByteCode.java
@@ -1,2 +1,14 @@
-package com.fb208.jcode;public class ByteCode {
+package com.fb208.jcode;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+public class ByteCode {
+    public static void main(String[] args) throws Exception {
+        ClassPool pool = ClassPool.getDefault();
+        CtClass ctClass = pool.get("com.thtf.eas.common.utils.MacAddressUtil");
+        CtMethod cm = ctClass.getDeclaredMethod( "validateRegistrationCode");
+        cm.setBody("return true;");
+        ctClass.writeFile("D:\\test");
+    }
 }
